d9ebbe83d1d19c7574e3d89a056458bdbda03192
Carlos Manias <carlmani@ie.ibm.com>
2017-07-26 14:30:23 +0100

    Added macosx install script

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
-#/bin/sh
+#!/usr/bin/env bash
+
+# Install Steve Francia's (spf13) Ultimate Vim distro
+curl http://j.mp/spf13-vim3 -L -o - | sh
+
+# Oh my tmux
+cd
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
 
 # Add local configuration files
 ln -s ~/dotfiles/.tmux.conf.local ~/.tmux.conf.local
@@ -6,4 +14,8 @@ ln -s ~/dotfiles/.vimrc.local ~/.vimrc.local
 ln -s ~/dotfiles/.vimrc.bundles.local ~/.vimrc.bundles.local
 ln -s ~/dotfiles/.zshrc.local ~/.zshrc.local
 
+#Git config
+cp ~/dotfiles/.gitconfig ~/.gitconfig
+
+#SSH client config
 cat ~/dotfiles/.ssh/config >> ~/.ssh/config
---
+++ macosx.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+# Check if we are on MacOS X
+if [ "$(uname)" != "Darwin" ]; then
+    exit 1 # Not MacOS X
+fi
+
+#Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#################################
+# COMMENT/UNCOMMENT AS REQUIRED #
+#################################
+
+#BREW
+#brew install aria2 # Download in parallel including bittorrent
+#brew install asciinema # Record animations from the terminal
+brew install coreutils # GNU core utils
+brew install cask # Cask
+#brew install docker
+#brew install git
+#brew install googler # Google search API command line
+#brew install gradle
+#brew install htop # system monitoring
+#brew install httpie # http tool
+#brew install jq # JSON tool
+#brew install jrnl # CLI Journal
+#brew install kpcli # KeePass CLI
+#brew install ledger # CLI Accounting system
+#brew install pandoc # Convert between file formats
+#brew install task # Taskwarrior
+#brew install tasksh # Taskwarrior shell
+#brew install the_silver_searcher # ag The silver searcher
+brew install tmux
+#brew install tor
+#brew install torsocks
+brew install vim
+#brew install weechat
+brew install wget
+#brew install youtube-dl # Youtube downloader
+brew install zsh
+brew install zsh-completions
+
+
+#CASK
+
+# Install hammerspoon
+#brew cask install hammerspoon
+
+# Awesome Hammerspoon configuration
+#git clone --depth 1 https://github.com/ashfinal/awesome-hammerspoon.git ~/.hammerspoon
+
+#brew cask install amethyst # Autoresize windows
+#brew cask install basictex # For LaTex support / convert to PDF with Pandoc
+#brew cask install bettertouchtool # Customize touch
+#brew cask install daisydisk # Disk usage visualizer
+#brew cask install dash # API documentation browser
+#brew cask install docker # Containers
+#brew cask install evernote # Notes
+#brew cask install itsycal # Calendar
+#brew cask install jumpcut # Clipboard buffering
+#brew cask install keka # Archiver tool
+#brew cask install macpass # KeePass GUI client
+#brew cask install outguess #Steganography
+#brew cask install postman # REST Client
+#brew cask install sublime-text3 # Text eitor
+#brew cask install veracrypt # Encryption
+#brew cask install virtualbox # Virtual images
+#brew cask install vlc # Media player
